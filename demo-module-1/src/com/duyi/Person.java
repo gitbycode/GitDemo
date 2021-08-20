116ac9eb0301980b82e5fca2ebf28eb5ba1525ec
gitbycode <404454986@qq.com>
2021-08-20 22:31:46 +0800

    尝试第二次更改

---
+++ demo-module-1/src/com/duyi/Person.java
@@ -0,0 +1,13 @@
+package com.duyi;
+
+/**
+ * @Auther: http: //www.zxy.com
+ * @Date: 2021/8/20
+ * @Description: com.duyi
+ * @version: 1.0
+ */
+
+public class Person {
+    public Person() {
+    }
+}
