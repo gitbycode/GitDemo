95f4c2e95ea1f3b2f16930dc3eb462a2a9ff2e25
gitbycode <404454986@qq.com>
2021-08-21 18:45:15 +0800

    尝试第四次更改

---
+++ demo-module-1/src/com/duyi/Teacher.java
@@ -0,0 +1,13 @@
+package com.duyi;
+
+/**
+ * @Auther: http: //www.zxy.com
+ * @Date: 2021/8/21
+ * @Description: com.duyi
+ * @version: 1.0
+ */
+
+public class Teacher {
+    public Teacher() {
+    }
+}
